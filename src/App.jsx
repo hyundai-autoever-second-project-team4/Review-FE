bc2c0c176e20cafb8460e4e13301e25a52dcf9b3
OhJeongHwan1 <wjdghks0316@naver.com>
2024-11-05 16:30:52 +0900

    feat & doc: Readme 정리 및 zustand persist devtool 테스트

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import Main from "./pages/Main";
 
 function App() {
   return (
     <div>
-      <Routes></Routes>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
       <ReactQueryDevtools initialIsOpen={true} />
     </div>
   );
