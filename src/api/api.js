ec53447de7e68854bb8c11dbcbbbe21fc2c68f19
yeon-dong <jae073@naver.com>
2024-11-14 16:20:08 +0900

    메인페이지에 리액트 쿼리 적용

--- src/api/api.js
+++ src/api/api.js
@@ -3,3 +3,28 @@ import { axiosInstance } from "./axiosInstance";
 export const getUserInfo = async () => {
   return await axiosInstance.get("/member");
 };
+
+export const fetchUserRecommendMovies = async () => {
+  const response = await axiosInstance.get("/movie/recommend");
+  return response.data.movies;
+};
+
+export const fetchTopRatedMovies = async () => {
+  const response = await axiosInstance.get("/movie/top-rated/weekly");
+  return response.data.movies;
+};
+
+export const fetchTopReviewedMovies = async () => {
+  const response = await axiosInstance.get("/movie/honor-board");
+  return response.data.movies;
+};
+
+export const fetchHotReview = async () => {
+  const response = await axiosInstance.get("/review/hot");
+  return response.data.reviewInfos;
+};
+
+export const fetchThearupHonorMovies = async () => {
+  const response = await axiosInstance.get("/movie/most-reviewed/weekly");
+  return response.data.movies;
+};
