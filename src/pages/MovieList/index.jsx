434425871761d82b11d699c2b76b4ad3c3ff0b3d
OhJeongHwan1 <wjdghks0316@naver.com>
2024-11-17 01:48:53 +0900

    feat: movieList prefetch 스켈레톤 적용 및 반응형 CustomPagination 개발

--- src/pages/MovieList/index.jsx
+++ src/pages/MovieList/index.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import MovieCard from "../../components/MovieCard/MovieCard";
 import Pagination from "@mui/material/Pagination";
 import { useNavigate, useParams } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import {
   BottomMargin,
   CardWrapper,
@@ -12,6 +12,9 @@ import {
   TitleWrap,
 } from "./MovieListStyle";
 import axios from "axios";
+import { smoothScrollTo } from "../../utils/smoothScrollTop.js";
+import MovieCardSkeleton from "../../components/MovieCard/MovieCardSkeleton.jsx";
+import CustomPagination from "../../components/CustomPagination/CustomPagination.jsx";
 
 const apiKey = import.meta.env.VITE_TMDB_API_KEY; // Vite 환경 변수에서 API 키를 가져옴
 const IMG_BASE_URL = "https://image.tmdb.org/t/p/w500"; // 이미지 베이스 URL
@@ -26,14 +29,15 @@ function MovieList() {
   const { type } = useParams();
   const navigate = useNavigate();
   const [pagination, setPagination] = useState(1); // 페이지 상태 추가
-
+  const queryClient = useQueryClient();
   const urlType = type === "nowPlaying" ? "now_playing" : "popular"; // URL 타입 결정
 
   // React Query로 데이터 fetching
-  const { data, isLoading, isError, error } = useQuery({
+  const { data, isLoading, isError } = useQuery({
     queryKey: ["movies", urlType, pagination],
     queryFn: () => fetchMovies({ urlType, pagination }),
     keepPreviousData: true, // 페이지네이션 시 이전 데이터 유지
+    gcTime: 60000, // 너무 긴 시간은 브라우저에 부담이 될 것이라고 판단하여 짧게 설정
   });
 
   // urlType이 변경될 때 pagination 초기화
@@ -41,44 +45,22 @@ function MovieList() {
     setPagination(1);
   }, [urlType]);
 
-  const movies = data?.results || [];
-  const totalPages = data ? (type === "nowPlaying" ? 50 : 100) : 0; // data가 없으면 0, 있으면 type에 따라 50 또는 100
+  useEffect(() => {
+    if (pagination < data?.totalPages) {
+      queryClient.prefetchQuery({
+        queryKey: ["movies", urlType, pagination + 1],
+        queryFn: () => fetchMovies({ urlType, pagination: pagination + 1 }),
+      });
+    }
+  }, [pagination]);
 
   const handlePageChange = (event, value) => {
     setPagination(value); // 페이지 변경
     smoothScrollTo(0, 500); // 500ms 동안 부드럽게 스크롤
   };
 
-  const smoothScrollTo = (targetY, duration) => {
-    const startY = window.scrollY;
-    const distance = targetY - startY;
-    const startTime = performance.now();
-
-    const animateScroll = (currentTime) => {
-      const elapsedTime = currentTime - startTime;
-      const progress = Math.min(elapsedTime / duration, 1); // 0에서 1까지의 비율
-
-      // easeInOutQuad easing function
-      const easing = (t) => {
-        return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
-      };
-
-      window.scrollTo(0, startY + distance * easing(progress));
-
-      if (progress < 1) {
-        requestAnimationFrame(animateScroll); // 애니메이션 계속 진행
-      }
-    };
-
-    requestAnimationFrame(animateScroll); // 애니메이션 시작
-  };
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
   if (isError) {
-    return <div>Error: {error.message}</div>;
+    return <div>Error: 데이터를 불러오는 데 실패했습니다.</div>;
   }
 
   return (
@@ -87,33 +69,33 @@ function MovieList() {
         <Title>{type === "nowPlaying" ? "현재상영작" : "인기영화"}</Title>
       </TitleWrap>
       <Container>
-        {movies.map((movie) => (
-          <CardWrapper key={movie.id}>
-            <MovieCard
-              onClick={() => navigate(`/movieDetail/${movie.id}`)}
-              title={movie.title}
-              poster={
-                movie.poster_path
-                  ? IMG_BASE_URL + movie.poster_path
-                  : "/images/no_img.png"
-              }
-              year={movie.release_date}
-              country={movie.original_language}
-              genre={movie.genre_ids}
-            />
-          </CardWrapper>
-        ))}
+        {!isLoading
+          ? data?.results.map((movie) => (
+              <CardWrapper key={movie.id}>
+                <MovieCard
+                  onClick={() => navigate(`/movieDetail/${movie.id}`)}
+                  title={movie.title}
+                  poster={
+                    movie.poster_path
+                      ? IMG_BASE_URL + movie.poster_path
+                      : "/images/no_img.png"
+                  }
+                  year={movie.release_date}
+                  country={movie.original_language}
+                  genre={movie.genre_ids}
+                />
+              </CardWrapper>
+            ))
+          : Array.from({ length: 20 }).map((_, index) => (
+              <CardWrapper key={index}>
+                <MovieCardSkeleton />
+              </CardWrapper>
+            ))}
       </Container>
-      <Pagination
-        count={totalPages} // 총 페이지 수
+      <CustomPagination
+        count={data ? (type === "nowPlaying" ? 50 : 100) : 0} // 총 페이지 수
         page={pagination} // 현재 페이지
         onChange={handlePageChange} // 페이지 변경 핸들러
-        siblingCount={5} // 현재 페이지 양쪽에 보여줄 버튼 수
-        sx={{
-          ".MuiPaginationItem-root.Mui-selected": {
-            backgroundColor: "#F2B705",
-          },
-        }}
       />
       <BottomMargin />
     </EveryContainer>
