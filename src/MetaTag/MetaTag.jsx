a185e449c86a479add7dca9bd0b406a925222389
yeon-dong <jae073@naver.com>
2024-11-18 16:23:46 +0900

    meta tag 이미지 url 수정

--- src/MetaTag/MetaTag.jsx
+++ src/MetaTag/MetaTag.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
+const IMG_BASE_URL = "https://image.tmdb.org/t/p/w500"; // 이미지 베이스 URL
 
 function MetaTag({ title, description, imgsrc, url }) {
   return (
@@ -13,12 +14,12 @@ function MetaTag({ title, description, imgsrc, url }) {
       <meta property="og:title" content={title} />
       <meta property="og:site_name" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content={imgsrc} />
+      <meta property="og:image" content={IMG_BASE_URL + `${imgsrc}`} />
       <meta property="og:url" content={url} />
 
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={imgsrc} />
+      <meta name="twitter:image" content={IMG_BASE_URL + `${imgsrc}`} />
 
       <link rel="canonical" href={url} />
     </Helmet>
